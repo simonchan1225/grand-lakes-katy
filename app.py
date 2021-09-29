bd0ec7c4c968ab65849a8942730287b319de687d
simonchan1225 <simonchan1225@gmail.com>
2021-09-28 22:24:40 -0500

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_the_map():
-    return render_template('grand_lakes.html')
+    return render_template('Grand_Lakes_2021_Fall_Garage_Sale.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
